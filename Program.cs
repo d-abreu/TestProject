e3402c8f79225d3b3316242785c578864675752f
d-abreu <daniel.ddabreu@gmail.com>
2018-05-20 18:33:14 +0200

    Added vscode and projects

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Dispatch;
+using Akka.Util;
+
+namespace Workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = ActorSystem.Create("TestSystem");
+            var actor = t.ActorOf(Props.Create<TestActor>());
+            actor.Tell(new LogMessage());
+            Console.ReadKey();
+        }
+    }
+
+    class TestActor : ReceiveActor
+    {
+        public TestActor()
+        {
+            Receive<LogMessage>(m => Console.WriteLine("Test"));
+        }
+    }
+
+    class LogMessage { }
+}
